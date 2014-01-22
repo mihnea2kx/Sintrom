387ffc4ac02e24924f069ca37fac787a9cd8d263
mihnea2kx <mihnea2kx@yahoo.com>
2014-01-22 23:40:33 +0200

    com4

---
+++ app.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$(document).ready(function() {
+   $('#inrbtn').click(function(){
+	   
+	   var inrval=$('input[name=user]').val();
+	   var inrdate=$('input[name=dateinr]').val();
+	   $('#inrhist').append('<div class="inrval">'+inrdate+ '  ----  ' +inrval+ '</div>');
+	   });
+	   
+	$('#dosagebtn').click(function(){
+	   
+	   var dosage=$('input[name=slider-1]').val();
+	   alert('Dosage is' + dosage + 'mg');
+	   });
+  
+});
