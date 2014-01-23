3b7acc99a6b795afe679718c5cd94c7307fb95a8
mihnea2kx <mihnea2kx@yahoo.com>
2014-01-23 19:00:28 +0200

    com6

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 // JavaScript Document
 $(document).ready(function() {
+   if(localStorage['linrval'],localStorage['linrdate']){
+	   $('#inrhist').prepend('<div class="inrval">'+localStorage['linrdate']+ '  ----  ' +localStorage['linrval']+ '</div>');
+	   };
+   
    $('#inrbtn').click(function(){
-	   
 	   var inrval=$('input[name=user]').val();
 	   var inrdate=$('input[name=dateinr]').val();
+	   localStorage.setItem('linrval',inrval);
+	   localStorage.setItem('linrdate',inrdate);
 	   $('#inrhist').prepend('<div class="inrval">'+inrdate+ '  ----  ' +inrval+ '</div>');
 	   });
-	   
+
+		
+		   
 	$('#dosagebtn').click(function(){
 	   
 	   var dosage=$('input[name=slider-1]').val();
-	   alert('Dosage is ' + dosage + ' mg');
+	   localStorage.setItem('ldosage',dosage);
+	   var storeddosage=localStorage.getItem('ldosage');
+	   alert('Dosage is ' + storeddosage + ' mg');
 	   });
-  
+  	
 });
